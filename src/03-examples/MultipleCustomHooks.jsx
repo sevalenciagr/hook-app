bba89c3543183e15733a2940a2cfa925530203ad
Sebastián Valencia G <sevalenciagr@gmail.com>
2023-08-30 14:35:48 -0500

    MultipleCustomHook ok

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,16 +1,30 @@
 import { useFetch } from "../hooks/useFetch"
 
-
 export const MultipleCustomHooks = () => {
-
     const { data, isLoading, hasError } = useFetch('https://rickandmortyapi.com/api/character/2')
 
     console.log({ data, isLoading, hasError });
 
-  return (
-    <>
-        <h1>Rick and Morty Characters</h1>
-        <hr />
-    </>
-  )
+    
+
+    return (
+        <>
+            <h1>Personajes de Rick and Morty</h1>
+            <hr />
+
+            {
+                isLoading
+                    ? (
+                        <div className="alert alert-info text-center">
+                            Cargando...
+                        </div>
+                    ) : (
+                        <blockquote className="blockquote text-end">
+                            <p className="mb-1">{ data.name }</p>
+                            <footer className="blockquote-footer">{ data.species }</footer>
+                        </blockquote>
+                    )
+            }
+        </>
+    )
 }
